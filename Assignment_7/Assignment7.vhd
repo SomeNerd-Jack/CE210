c0583058beaea124a4ddfb0208c5d00a70d3b61c
SomeNerd-Jack <37645049+SomeNerd-Jack@users.noreply.github.com>
2021-09-01 13:31:05 -0400

    Mostly finished but still need to double check

--- Assignment_7/Assignment7.vhd
+++ Assignment_7/Assignment7.vhd
@@ -30,16 +30,17 @@ ARCHITECTURE Behavior OF Assignment7 IS
    SIGNAL Z0 : STD_LOGIC_VECTOR(4 DOWNTO 0);   -- used for BCD addition
    SIGNAL T0 : STD_LOGIC_VECTOR(4 DOWNTO 0);   -- used for BCD addition
 BEGIN
-   A <= SW(7 DOWNTO 4);
-   B <= SW(3 DOWNTO 0);
-   Cin <= SW(8);
+   A <= SW(7 DOWNTO 4);	-- A value should be entered with switches 7-4 (inclusive)
+   B <= SW(3 DOWNTO 0); -- B value should be entered with switches 3-0 (inclusive)
+   Cin <= SW(8);			-- Carry-in value should be entered with switch 8
    
-   -- add lower two bcd digits. Result is five bits: C1,S0
+   -- add lower two bcd digits. Result is five bits: C1,S0 ---------------------------------------- I don't understand this
    T0 <= ('0' & A) + ('0' & B) + Cin;
+	
    PROCESS (T0)
    BEGIN
-		IF(T0 > 9) THEN
-			Z0 <= "00010";
+		IF(T0 > 9) THEN -- If the 
+			Z0 <= "01010"; --Assign the value to decimal 10
 			C1 <= '1';
 		ELSE
 			Z0 <= "00000";
@@ -50,13 +51,27 @@ BEGIN
 		S0 <= T0 - Z0;
 		S1 <= C1;
 	END PROCESS;
-
---Code is not shown -----------------------------------------
    
    -- drive the displays through 7-seg decoders
-   digit3: bcd7seg PORT MAP (A, HEX5);
-   
---Code is not shown -----------------------------------------
+   displayA:	bcd7seg PORT MAP (A, HEX5);
+	displayB:	bcd7seg PORT MAP (B, HEX3);
+	
+	
+	PROCESS(S1)
+	BEGIN
+		IF(S1 > '0') THEN
+			HEX1 <= "1001111";
+		ELSE
+			HEX1 <= "0000001";
+		END IF;
+	END PROCESS;
+	
+	--displayS1:	bcd7seg PORT MAP (S1, HEX1);
+	
+	displayS0:	bcd7seg PORT MAP (S0(3 DOWNTO 0), HEX0);
+	
+	turnOffDisplay1: bcd7seg PORT MAP ("1111", HEX2);
+	turnOffDisplay2: bcd7seg PORT MAP ("1111", HEX4);
    
 END Behavior;
          
@@ -85,18 +100,18 @@ BEGIN
    --
    PROCESS (bcd)
    BEGIN
-	CASE bcd IS 					-- 0123456
-		WHEN "0000" => display <= "1111110" ;
-		WHEN "0001" => display <= "0110000" ;
-		WHEN "0010" => display <= "1101101" ;
-		WHEN "0011" => display <= "1111001" ;
-		WHEN "0100" => display <= "0110011" ;
-		WHEN "0101" => display <= "1011011" ;
-		WHEN "0110" => display <= "1011111" ;
-		WHEN "0111" => display <= "1110000" ;
-		WHEN "1000" => display <= "1111111" ;
-		WHEN "1001" => display <= "1110011" ;
-		WHEN OTHERS => display <= "-------" ;
+	CASE bcd IS 					-- 0123456 (These are the corresponding digit bits)
+		WHEN "0000" => display <= "0000001" ; -- The display is active low, so 1 is off and 0 is on
+		WHEN "0001" => display <= "1001111" ;
+		WHEN "0010" => display <= "0010010" ;
+		WHEN "0011" => display <= "0000110" ;
+		WHEN "0100" => display <= "1001100" ;
+		WHEN "0101" => display <= "0100100" ;
+		WHEN "0110" => display <= "0100000" ;
+		WHEN "0111" => display <= "0001111" ;
+		WHEN "1000" => display <= "0000000" ;
+		WHEN "1001" => display <= "0001100" ;
+		WHEN OTHERS => display <= "1111111" ; -- If the input is any other value, turn off the display
 	END CASE ;
    END PROCESS;
 END Behavior;
