3a3abea1fd195d7b39e4a84ad87760c980464d00
SomeNerd-Jack <37645049+SomeNerd-Jack@users.noreply.github.com>
2021-09-02 08:02:31 -0400

    Finished Assignment 7
    Nothing left to do. I commented it and it works as intended. The quiz was rough though

--- Assignment_7/Assignment7.vhd
+++ Assignment_7/Assignment7.vhd
@@ -12,7 +12,7 @@ USE ieee.std_logic_unsigned.all;
 
 ENTITY Assignment7 IS 
    PORT ( SW : IN STD_LOGIC_VECTOR(8 DOWNTO 0);
-          HEX5, HEX4, HEX3, HEX2, HEX1, HEX0 : OUT STD_LOGIC_VECTOR(0 TO 6));  
+          HEX5, HEX4, HEX3, HEX2, HEX1, HEX0 : OUT STD_LOGIC_VECTOR(0 TO 6));
 END Assignment7;
 
 ARCHITECTURE Behavior OF Assignment7 IS
@@ -33,44 +33,34 @@ BEGIN
    A <= SW(7 DOWNTO 4);	-- A value should be entered with switches 7-4 (inclusive)
    B <= SW(3 DOWNTO 0); -- B value should be entered with switches 3-0 (inclusive)
    Cin <= SW(8);			-- Carry-in value should be entered with switch 8
-   
-   -- add lower two bcd digits. Result is five bits: C1,S0 ---------------------------------------- I don't understand this
-   T0 <= ('0' & A) + ('0' & B) + Cin;
+
+   -- add lower two bcd digits. Result is five bits: C1,S0
+   T0 <= ('0' & A) + ('0' & B) + Cin; -- This is the total addition statement, and ('0' & X) just APPENDS a 0 to X
 	
    PROCESS (T0)
    BEGIN
-		IF(T0 > 9) THEN -- If the 
-			Z0 <= "01010"; --Assign the value to decimal 10
-			C1 <= '1';
+		IF(T0 > 9) THEN 	-- If the result of the addition is 10 or more, it needs to be carried
+			Z0 <= "01010"; -- Assign value of the carry to decimal 10
+			C1 <= '1';		-- Carry bit
 		ELSE
-			Z0 <= "00000";
-			C1 <= '0';
-	  
+			Z0 <= "00000";	-- Assign value of the carry to decimal 0
+			C1 <= '0';		-- Carry bit
 		END IF;
 
-		S0 <= T0 - Z0;
-		S1 <= C1;
+		S0 <= T0 - Z0; -- Subtract the carry value from the total additive result to get the least significant digit
+		S1 <= C1; -- Whether or not the most significant digit should be "1" or "0"
 	END PROCESS;
    
-   -- drive the displays through 7-seg decoders
-   displayA:	bcd7seg PORT MAP (A, HEX5);
+   -- Display values through 7-seg decoders
+   displayA:	bcd7seg PORT MAP (A, HEX5); -- Display A and B
 	displayB:	bcd7seg PORT MAP (B, HEX3);
 	
 	
-	PROCESS(S1)
-	BEGIN
-		IF(S1 > '0') THEN
-			HEX1 <= "1001111";
-		ELSE
-			HEX1 <= "0000001";
-		END IF;
-	END PROCESS;
+	displayS1: bcd7seg PORT MAP ("000" & S1, HEX1); -- Display the most significant digit of the result
 	
-	--displayS1:	bcd7seg PORT MAP (S1, HEX1);
+	displayS0:	bcd7seg PORT MAP (S0(3 DOWNTO 0), HEX0);	--Display the least significant digit of the result
 	
-	displayS0:	bcd7seg PORT MAP (S0(3 DOWNTO 0), HEX0);
-	
-	turnOffDisplay1: bcd7seg PORT MAP ("1111", HEX2);
+	turnOffDisplay1: bcd7seg PORT MAP ("1111", HEX2); -- Turn off displays 2 and 4 to clean up the output
 	turnOffDisplay2: bcd7seg PORT MAP ("1111", HEX4);
    
 END Behavior;
@@ -100,18 +90,18 @@ BEGIN
    --
    PROCESS (bcd)
    BEGIN
-	CASE bcd IS 					-- 0123456 (These are the corresponding digit bits)
-		WHEN "0000" => display <= "0000001" ; -- The display is active low, so 1 is off and 0 is on
-		WHEN "0001" => display <= "1001111" ;
-		WHEN "0010" => display <= "0010010" ;
-		WHEN "0011" => display <= "0000110" ;
-		WHEN "0100" => display <= "1001100" ;
-		WHEN "0101" => display <= "0100100" ;
-		WHEN "0110" => display <= "0100000" ;
-		WHEN "0111" => display <= "0001111" ;
-		WHEN "1000" => display <= "0000000" ;
-		WHEN "1001" => display <= "0001100" ;
-		WHEN OTHERS => display <= "1111111" ; -- If the input is any other value, turn off the display
-	END CASE ;
+		CASE bcd IS 					-- 0123456 (These are the corresponding digit bits)
+			WHEN "0000" => display <= "0000001" ; -- The display is active low, so 1 is off and 0 is on
+			WHEN "0001" => display <= "1001111" ;
+			WHEN "0010" => display <= "0010010" ;
+			WHEN "0011" => display <= "0000110" ;
+			WHEN "0100" => display <= "1001100" ;
+			WHEN "0101" => display <= "0100100" ;
+			WHEN "0110" => display <= "0100000" ;
+			WHEN "0111" => display <= "0001111" ;
+			WHEN "1000" => display <= "0000000" ;
+			WHEN "1001" => display <= "0001100" ;
+			WHEN OTHERS => display <= "1111111" ; -- If the input is any other value (>9 or <0), turn off the display
+		END CASE;
    END PROCESS;
 END Behavior;
